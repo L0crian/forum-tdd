1b01dfce6420a81f9e32fa7e4bc00c57f98b8754
L0crian <mailjustmy@gmail.com>
2017-07-13 03:36:49 +0300

    Popular sort, global scope for replies count

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,13 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+<style>
+    body {padding-bottom: 100px;}
+    .level{ display: flex; align-items: center}
+    .flex{flex: 1;}
+
+</style>
+
 <body>
 <div id="app">
     <nav class="navbar navbar-default navbar-static-top">
@@ -40,12 +47,11 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                            aria-expanded="false">Browse <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            &nbsp;<li><a href="/threads">All Threads</a></li>
+                        <ul class="dropdown-menu"><li><a href="/threads">All Threads</a></li>
                             @if(auth()->check())
-                                &nbsp;
                                 <li><a href="/threads?by={{auth()->user()->name}}">My Threads</a></li>
                             @endif
+                            <li><a href="/threads?popular=1">Popular Threads</a></li>
                         </ul>
                     </li>
                     <li class="dropdown">
