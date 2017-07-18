9cb1056d0738e74aadf125213d5ea82226f25344
L0crian <mailjustmy@gmail.com>
2017-07-18 03:37:23 +0300

    Vue reply component

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 
 Route::post('replies/{reply}/favorites', 'FavoritesController@store');
+Route::patch('/replies/{reply}', 'RepliesController@update');
 Route::delete('/replies/{reply}', 'RepliesController@destroy');
 
 Route::get('/profiles/{user}', 'ProfilesController@show');
